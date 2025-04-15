2ca76a8bee1628a5e377d4afd8fda209ff0fc346
madousang <madous431@gmail.com>
2025-04-15 03:22:08 +0000

    Add sum logic, controller and unit test

--- src/test/java/com/itguy/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/itguy/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,16 @@
 package com.itguy.calculator;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
+	private Calculator calculator = new Calculator();
 	@Test
-	void contextLoads() {
+	public void testSum() {
+		assertEquals(5, calculator.sum(2, 3));
 	}
 
 }
