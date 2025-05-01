352a9ff471d2dea53d5fb7875925a4ae13280ddc
Madou <madous431@gmail.com>
2025-05-01 02:14:24 +0000

    add cacheconfig file, link calculator to redis cache

--- src/main/java/com/itguy/calculator/Calculator.java
+++ src/main/java/com/itguy/calculator/Calculator.java
@@ -1,9 +1,10 @@
 package com.itguy.calculator;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.cache.annotation.Cacheable;
 @Service
 public class Calculator {
+    @Cacheable("sum")
     int sum(int a, int b) {
         return a + b;
     }
